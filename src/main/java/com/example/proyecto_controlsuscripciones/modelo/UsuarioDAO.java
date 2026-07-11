e332f5da942475e171fa22f6230d10df055d0130
Lizbeth593 <lizbeth59312@gmail.com>
2026-07-10 20:15:46 -0500

    Se implemnto nuevos metodos para cumplir la funcionalidad de la ventana clientes

--- src/main/java/com/example/proyecto_controlsuscripciones/modelo/UsuarioDAO.java
+++ src/main/java/com/example/proyecto_controlsuscripciones/modelo/UsuarioDAO.java
@@ -1,6 +1,8 @@
 package com.example.proyecto_controlsuscripciones.modelo;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
     private Conexion conexion = new Conexion();
@@ -41,5 +43,92 @@ public class UsuarioDAO {
             System.out.println("Error al buscar: " + e.getMessage());
         }
         return null;
+    }
+
+    //Para la ventana clientes
+    //MOSTRAR
+
+    public List<Usuario> listarUsuarios() {
+
+        List<Usuario> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM usuarios";
+
+        try (Connection conn = conexion.conectarMySQL();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+
+                Usuario u = new Usuario();
+
+                u.setId_usuario(rs.getInt("id_usuario"));
+                u.setUsuario(rs.getString("usuario"));
+                u.setCorreo(rs.getString("correo"));
+                u.setRol(rs.getString("rol"));
+
+                lista.add(u);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return lista;
+    }
+
+    //BUSCAR
+
+    public Usuario buscarPorId(int id) {
+
+        String sql = "SELECT * FROM usuarios WHERE id_usuario = ?";
+
+        try (Connection conn = conexion.conectarMySQL();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                Usuario u = new Usuario();
+
+                u.setId_usuario(rs.getInt("id_usuario"));
+                u.setUsuario(rs.getString("usuario"));
+                u.setCorreo(rs.getString("correo"));
+                u.setRol(rs.getString("rol"));
+
+                return u;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    //ELIMINAR
+
+
+    public boolean eliminarUsuario(int id) {
+
+        String sql = "DELETE FROM usuarios WHERE id_usuario = ?";
+
+        try (Connection conn = conexion.conectarMySQL();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+        return false;
     }
 }
